dbc99e0708dcd77cf927bdc49ef8bdcee114f26d
Roman Zhelezniak <zhelezniak.r@ajax.systems>
2026-02-24 17:39:22 +0200

    Changed middleware logging

--- logger/middlewares_logging.py
+++ logger/middlewares_logging.py
@@ -1,11 +1,14 @@
 from aiogram import BaseMiddleware
-from aiogram.types import Message
+from aiogram.types import Message, TelegramObject
 import logging
 
 logger = logging.getLogger("UserActivity")
 
 
 class LoggingMiddleware(BaseMiddleware):
-    async def __call__(self, handler, event: Message, data):
-        logger.info(f"User {event.from_user.id} sent: {event.text}")
+    async def __call__(self, handler, event: TelegramObject, data):
+        if isinstance(event, Message):
+            msg : Message = event
+            user_id = msg.from_user.id if msg.from_user else "Unknown"
+            logger.info(f"User {user_id} sent: {msg.text}")
         return await handler(event, data)
