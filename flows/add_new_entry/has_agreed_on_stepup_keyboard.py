c5e7775bfdeb5ea677aa07dbf66da28b0b9d3328
Roman Zhelezniak <zhelezniak.r@ajax.systems>
2026-09-13 22:07:28 +0300

    New branches for add entry flow and some sorting

---
+++ flows/add_new_entry/has_agreed_on_stepup_keyboard.py
@@ -0,0 +1,23 @@
+from enum import Enum
+from aiogram.types import ReplyKeyboardMarkup
+from aiogram.utils.keyboard import ReplyKeyboardBuilder
+
+from commands import BotCommands
+
+
+class HasAgreedOnStepupReplies(Enum):
+    Yes = "Так"
+    No = "Ні"
+
+
+class HasAgreedOnStepupKeyboard(ReplyKeyboardBuilder): 
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.button(text=HasAgreedOnStepupReplies.Yes.value)
+        self.button(text=HasAgreedOnStepupReplies.No.value)
+        self.button(text=BotCommands.Cancel.value)
+        self.adjust(2) 
+
+    def get_markup(self, resize_keyboard: bool = True, input_field_placeholder: str = "Оберіть пункт меню...") -> ReplyKeyboardMarkup:
+        return super().as_markup(resize_keyboard=resize_keyboard, input_field_placeholder=input_field_placeholder)
+
